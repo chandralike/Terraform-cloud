a2eb015572dff329d90456172221e7d0ddde9f61
chandralike <110821341+chandralike@users.noreply.github.com>
2022-08-28 13:18:40 +0530

    Update variables.tf
    third commit

--- variables.tf
+++ variables.tf
@@ -17,3 +17,12 @@ variable "client_secret"{
 type=string
 description = "This is the client secret"
 }
+resource "azurerm_subnet" "subnetA" {    
+    name                 = "SubnetA"
+    resource_group_name  = azurerm_resource_group.resourcegrp.name
+    virtual_network_name = azurerm_virtual_network.network.name
+    address_prefixes     = [cidrsubnet(local.virtual_network.address_space,8,2)]
+    depends_on = [
+      azurerm_virtual_network.network
+    ]
+}
